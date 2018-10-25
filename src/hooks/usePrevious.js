c6e8f5502ac0a664e920ceec988cb18b625131b8
Vitor Balocco <vitorbal@gmail.com>
2018-10-26 00:40:56 +0200

    feat: Add usePrevious hook
    Add our first custom hook, `usePrevious`

---
+++ src/hooks/usePrevious.js
@@ -0,0 +1,12 @@
+// @flow
+import { useEffect, useRef } from 'react';
+
+export default function usePrevious(value) {
+  const ref = useRef();
+
+  useEffect(() => {
+    ref.current = value;
+  });
+
+  return ref.current;
+}
---
+++ src/index.js
@@ -0,0 +1,2 @@
+// @flow
+export { default as usePrevious } from './hooks/usePrevious';
